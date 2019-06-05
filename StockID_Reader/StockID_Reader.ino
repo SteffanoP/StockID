5c9e2479894f6f745153b0d81addcd226adaacde
Steffano Pereira <steffanose@gmail.com>
2019-06-04 21:35:48 -0300

    Adaptação para padrões de Fabricação(04/06)
    Pinagem adaptada para código base de funcionamento

--- StockID_Reader/StockID_Reader.ino
+++ StockID_Reader/StockID_Reader.ino
@@ -1,47 +1,33 @@
-//Programa : RFID - Controle de Acesso leitor RFID
-//Autor : FILIPEFLOP
-
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define led_vermelho 2
-#define led_verde 3
+#define PIN_SDA 3
+#define PIN_RST 4
 
-#define SS_PIN 10
-#define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+MFRC522 mfrc522(PIN_SDA, PIN_RST);   // Create MFRC522 instance.
 
-char constantes[2][11] = {{"FF FF FF FF"},{"56 BB 59 90"}};
-bool estadoAnteriorOcupado = 0;
+char constantes[2][12] = {{"FF FF FF FF"},{"56 BB 59 90"}};
 
 void setup() {
   Serial.begin(9600);   // Inicia a serial
-  SPI.begin();      // Inicia  SPI bus
+  SPI.begin();      // Inicia SPI bus
   mfrc522.PCD_Init();   // Inicia MFRC522
-  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+  mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);  //Set da antena para ganho máximo
   Serial.println("Aproxime o seu cartao do leitor...");
   Serial.println();
-  Estado_livre();
 }
 
 void loop() {
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent())
-  {
-    if(estadoAnteriorOcupado == false) {
-      Estado_livre();
-    }
-    estadoAnteriorOcupado = false;
+  // Procura por uma nova tag
+  if (!mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial())
-  {
-    Estado_livre();
+  // Realiza a leitura da tag por meio do protocolo Serial
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
-  //Mostra UID na serial
-  Serial.print("UID da tag :");
+  //Mostra o ID na Serial
+  Serial.print("ID da tag :");
   String conteudo= "";
   byte letra;
   for (byte i = 0; i < mfrc522.uid.size; i++) {
@@ -51,22 +37,9 @@ void loop() {
      conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   Serial.println();
-  Serial.println(constantes[1]);
   conteudo.toUpperCase();
-  if (conteudo.substring(1) == constantes[1])  {
-    Estado_ocupado();
-    delay(500);
+
+  if (conteudo.substring(1) == constantes[1]) {
+    Serial.println("Tag Reconhecida");
   }
-}
-
-void Estado_ocupado() {
-  digitalWrite(led_vermelho, HIGH);
-  digitalWrite(led_verde, LOW);
-  Serial.println("Estado Ocupado");
-  estadoAnteriorOcupado = true;
-}
-
-void Estado_livre() {
-  digitalWrite(led_verde, HIGH);
-  digitalWrite(led_vermelho, LOW);
 }
